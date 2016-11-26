21affb0e7fd8034e7c7e00c6f806506bea641765
linyacool <xxbbb@vip.qq.com>
2016-11-26 13:36:44 +0800

    upload

---
+++ decode.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+import cv2
+import numpy as np
+import random
+import os
+from argparse import ArgumentParser
+ALPHA = 5
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--original', dest='ori', required=True)
+    parser.add_argument('--image', dest='img', required=True)
+    parser.add_argument('--result', dest='res', required=True)
+    parser.add_argument('--alpha', dest='alpha', default=ALPHA)
+    return parser
+
+
+def main():
+    parser = build_parser()
+    options = parser.parse_args()
+    ori = options.ori
+    img = options.img
+    res = options.res
+    alpha = options.alpha
+    if not os.path.isfile(ori):
+        parser.error("original image %s does not exist." % ori)
+    if not os.path.isfile(img):
+        parser.error("image %s does not exist." % img)
+    decode(ori, img, res, alpha)
+
+
+def decode(ori_path, img_path, res_path, alpha):
+    ori = cv2.imread(ori_path)
+    img = cv2.imread(img_path)
+    ori_f = np.fft.fft2(ori)
+    img_f = np.fft.fft2(img)
+    height, width = ori.shape[0], ori.shape[1]
+    watermark = (ori_f - img_f) / alpha
+    watermark = np.real(watermark)
+    res = np.zeros(watermark.shape)
+    random.seed(height + width)
+    x = range(height / 2)
+    y = range(width)
+    random.shuffle(x)
+    random.shuffle(y)
+    for i in range(height / 2):
+        for j in range(width):
+            res[x[i]][y[j]] = watermark[i][j]
+    cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+
+if __name__ == '__main__':
+    main()
---
+++ encode.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+import cv2
+import numpy as np
+import random
+import os
+from argparse import ArgumentParser
+ALPHA = 5
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--image', dest='img', required=True)
+    parser.add_argument('--watermark', dest='wm', required=True)
+    parser.add_argument('--result', dest='res', required=True)
+    parser.add_argument('--alpha', dest='alpha', default=ALPHA)
+    return parser
+
+
+def main():
+    parser = build_parser()
+    options = parser.parse_args()
+    img = options.img
+    wm = options.wm
+    res = options.res
+    alpha = options.alpha
+    if not os.path.isfile(img):
+        parser.error("image %s does not exist." % img)
+    if not os.path.isfile(wm):
+        parser.error("watermark %s does not exist." % wm)
+    encode(img, wm, res, alpha)
+
+
+def encode(img_path, wm_path, res_path, alpha):
+    img = cv2.imread(img_path)
+    img_f = np.fft.fft2(img)
+    height, width, channel = np.shape(img)
+    watermark = cv2.imread(wm_path)
+    wm_height, wm_width = watermark.shape[0], watermark.shape[1]
+    x, y = range(height / 2), range(width)
+    random.seed(height + width)
+    random.shuffle(x)
+    random.shuffle(y)
+    tmp = np.zeros(img.shape)
+    for i in range(height / 2):
+        for j in range(width):
+            if x[i] < wm_height and y[j] < wm_width:
+                tmp[i][j] = watermark[x[i]][y[j]]
+                tmp[height - 1 - i][width - 1 - j] = tmp[i][j]
+    res_f = img_f + alpha * tmp
+    res = np.fft.ifft2(res_f)
+    res = np.real(res)
+    cv2.imwrite(res_path, res, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+if __name__ == '__main__':
+    main()
