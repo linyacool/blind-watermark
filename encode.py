a3f954c6ead3ee1cc44f59ab74c9d27e7f8b1c34
sotex <ymwh@foxmail.com>
2019-03-11 12:20:42 +0800

    python3语法适配，decode输入图像大小调整一致

--- encode.py
+++ encode.py
@@ -36,12 +36,12 @@ def encode(img_path, wm_path, res_path, alpha):
     height, width, channel = np.shape(img)
     watermark = cv2.imread(wm_path)
     wm_height, wm_width = watermark.shape[0], watermark.shape[1]
-    x, y = range(height / 2), range(width)
+    x, y = list(range(height // 2)), list(range(width))
     random.seed(height + width)
     random.shuffle(x)
     random.shuffle(y)
     tmp = np.zeros(img.shape)
-    for i in range(height / 2):
+    for i in range(height // 2):
         for j in range(width):
             if x[i] < wm_height and y[j] < wm_width:
                 tmp[i][j] = watermark[x[i]][y[j]]
